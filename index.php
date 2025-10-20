a0caaf3c9ab346388f978e4394ba6ae0204e6f51
Tomas <tomikm052@gmail.com>
2025-10-20 20:22:31 +0200

    pridani index.php a directory styles

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel = "stylesheet" href = "styles/home.css"
+</head>
+<body>
+    <hr>
+        <h1>CAU TADO</h1>
+    <hr>
+    <p>Tohle bude home stranka<br>
+        <i>Na home strance bude navbar a vylistovani nejnovejsich nabidek<br></i>
+        <b>Uprav kdyztak tenhle tucnej text at jsem si jistej ze git repo fr funguje</b>
+    </p>
+</body>
+</html>
