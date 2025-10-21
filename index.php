475edfd2ba0cb50b26055a133d8864600700b47f
Tomas <tomikm052@gmail.com>
2025-10-21 17:02:22 +0200

    nepushnul se index.php?

--- index.php
+++ index.php
@@ -7,23 +7,17 @@
     <link rel = "stylesheet" href = "styles/home.css"
 </head>
 <body>
-    <hr>
-        <h1>CAU TADO</h1>
-    <hr>
-    <p>Tohle bude home stranka<br>
-        <i>Na home strance bude navbar a vylistovani nejnovejsich nabidek<br></i>
-        <b>Uprav kdyztak tenhle tucnej text at jsem si jistej ze git repo fr funguje</b>
-        <b>CAUUUU</b>
-    </p>
-    
-    
-    <?php
-    for($i = 0; $i < 10; $i++){
-        echo($i . "<br>");
-    }
+    <div class = 'navbar'>
+        <p>Navbar</p>
+    </div>
+
+    <div class = 'content'>
+        <p id = 'tmp'>Content</p>
+    </div>
 
 
-    ?>
-
+    <footer class = 'footer'>
+        <p>Footer</p>
+    </footer>
 </body>
 </html>
