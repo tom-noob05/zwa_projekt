b27032167d0d47df4a3ff7c226db60b2f61b5736
Tomas <tomikm052@gmail.com>
2025-11-01 14:38:43 +0100

    priprava navbaru a login stranky

---
+++ includes/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+    <a  href="/"">Community Marketplace</a>
+    <select id="categories-dropdown">
+        <option>Kategorie 0<option>
+        <option>Kategorie 1<option>
+        <option>Kategorie 2<option>    
+    </select>
+    <a href="login.php"><button id="login">Log In</butotn></a>
+</nav>
--- index.php
+++ index.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel = "stylesheet" href = "styles/home.css"
+    <link rel = "stylesheet" href = "/public/styles/home.css">
+    <link rel = "stylesheet" href = "/public/styles/navbar.css">
 </head>
 <body>
-    <div class = 'navbar'>
-        <p>Navbar</p>
-    </div>
+    <!-- <div class = 'navbar'>
+        <a href="index.php">Community Marketplace</a>
+    </div> -->
+    <?php include 'includes/navbar.php';?>
 
     <div class = 'content'>
         <p id = 'tmp'>Content</p>
