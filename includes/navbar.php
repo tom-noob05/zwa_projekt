12aee756c4e8ca070b5853f61ae65ae1f643723e
tadeas krasa <tada.krasa@gmail.com>
2025-11-19 14:47:43 +0100

    navbar zmena

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 // require_once '../config/init.php';
 ?>
 
 <link rel="stylesheet" href="public/styles/navbar.css">
 <nav class="navbar">
     <section class="links">
-    <a href="/" class="nav-link">Market place</a>
-    
+        <a href="/" class="nav-link">Market place</a>
+
         <div class="dropdown">
             <a href="#" class="nav-link">Kategorie</a>
             <div class="dropdown-content">
@@ -16,26 +16,24 @@
             </div>
         </div>
     </section>
-    <?php 
-        if (!empty($_SESSION['user']))
-        {
-        ?>
-            <div class='account-button'>
-                <a href='/pages/profile.php' id='username'><?php echo($_SESSION['user']['username']);?></a>
-                <button id='logoutbtn' class='navbar-button' onclick="confirmLogout()">Log Out</button>
-            </div>
-
-        <?php
-        }else
-        { ?>
-            <div class='account-button'>
-                <button id="loginbtn" class='navbar-button' onclick="location.href='/pages/login.php'">Log In</button>
-            </div>
-        <?php
-        }
-    
+    <?php
+    if (!empty($_SESSION['user'])) {
     ?>
-    
+        <div class='account-button'>
+            <a href='/pages/profile.php' id='username'><?php echo ($_SESSION['user']['username']); ?></a>
+            <button id='logoutbtn' class='navbar-button' onclick="confirmLogout()">Log Out</button>
+        </div>
+
+    <?php
+    } else { ?>
+        <div class='account-button' onclick="location.href='/pages/login.php'">
+            <button id="loginbtn" class='navbar-button'>Log In</button>
+        </div>
+    <?php
+    }
+
+    ?>
+
 </nav>
 
 <script src='public/js/navbar.js'></script>
