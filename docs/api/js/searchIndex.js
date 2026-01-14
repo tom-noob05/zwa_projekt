2ea1cc17d415aca92af38a9dad9cca4185fd64b7
tada.krasa <tada.krasa@gmail.com>
2026-01-14 20:51:00 +0100

    vygenerovana dokumentace

---
+++ docs/api/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\testFunction\u0028\u0029",
+            "name": "testFunction",
+            "summary": "Simple\u0020debug\u0020helper\u0020that\u0020prints\u0020a\u0020message\u0020to\u0020the\u0020page.",
+            "url": "namespaces/default.html#function_testFunction"
+        },                {
+            "fqsen": "\\destroySession\u0028\u0029",
+            "name": "destroySession",
+            "summary": "Destroys\u0020the\u0020current\u0020session\u0020and\u0020clears\u0020session\u0020cookie\u0020data.",
+            "url": "namespaces/default.html#function_destroySession"
+        },                {
+            "fqsen": "\\redirectIfLoggedIn\u0028\u0029",
+            "name": "redirectIfLoggedIn",
+            "summary": "Redirects\u0020the\u0020current\u0020request\u0020to\u0020the\u0020homepage\u0020when\u0020an\u0020authenticated\nuser\u0020is\u0020already\u0020logged\u0020in.\u0020Useful\u0020to\u0020prevent\u0020showing\u0020login\/register\npages\u0020to\u0020authenticated\u0020users.",
+            "url": "namespaces/default.html#function_redirectIfLoggedIn"
+        },                {
+            "fqsen": "\\sanitize_img_path\u0028\u0029",
+            "name": "sanitize_img_path",
+            "summary": "",
+            "url": "namespaces/default.html#function_sanitize_img_path"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
